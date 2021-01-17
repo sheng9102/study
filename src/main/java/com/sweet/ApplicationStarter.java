5fa66a9b3907deaf10a8024f08e782125c24f9d5
tianhaichao <tianhaichao91@163.com>
2021-01-17 17:17:00 +0800

    package

--- src/main/java/com/sweet/ApplicationStarter.java
+++ src/main/java/com/sweet/ApplicationStarter.java
@@ -3,15 +3,23 @@ package com.sweet;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @MapperScan("com.sweet.mapper")
 @EnableAspectJAutoProxy
-public class ApplicationStarter {
+public class ApplicationStarter extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApplicationStarter.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationStarter.class, args);
+    }
+
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 
 }
